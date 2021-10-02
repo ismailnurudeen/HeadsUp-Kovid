8b0d7d01b90fbc585e259a21f1d5c577d1dc3e29
TheDaniels <USER@TheDaniels.thedanielsdev>
2021-10-02 17:48:11 +0100

    intro to assessments pages created

---
+++ lib/assessement pages/page_two.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PageTwo extends StatefulWidget {
+  PageTwo({Key? key}) : super(key: key);
+
+  @override
+  _PageTwoState createState() => _PageTwoState();
+}
+
+class _PageTwoState extends State<PageTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/models/cardmodel.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CardModel {
+  String image, name;
+  Color color;
+
+  CardModel({required this.image, required this.name, required this.color});
+}
