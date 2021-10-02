654cb541566e2fedd7e890f00a76d2b2d13ac327
TheDaniels <USER@TheDaniels.thedanielsdev>
2021-10-02 15:38:12 +0100

    assessment page build started

---
+++ lib/assessement pages/page_one.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageOne extends StatefulWidget {
+  PageOne({Key? key}) : super(key: key);
+
+  @override
+  _PageOneState createState() => _PageOneState();
+}
+
+class _PageOneState extends State<PageOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
